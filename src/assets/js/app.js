b6f7edcee6f1b514913541819dc006486dd65f2b
Lisa Wells <lisawells999@gmail.com>
2016-02-12 15:18:55 -0500

    add a hamburger, but it's on the left

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -41,6 +41,31 @@ $(document).ready(function() {
 		});
 	});
 
+	// SmartMenus Hamburger
+	$(function() {
+	  var $mainMenuState = $('#main-menu-state');
+	  if ($mainMenuState.length) {
+	    // animate mobile menu
+	    $mainMenuState.change(function(e) {
+	      var $menu = $('#main-menu');
+	      if (this.checked) {
+	        $menu.hide().slideDown(250, function() { $menu.css('display', ''); });
+	      } else {
+	        $menu.show().slideUp(250, function() { $menu.css('display', ''); });
+	      }
+	    });
+	    // hide mobile menu beforeunload
+	    $(window).bind('beforeunload unload', function() {
+	      if ($mainMenuState[0].checked) {
+	        $mainMenuState[0].click();
+	      }
+	    });
+	  }
+	});
+
+
+	
+
 
 }); //end document ready
 
